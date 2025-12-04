b37765d454c9195e1274c50c1b3f63998375da38
User0600 <143862592+User0600@users.noreply.github.com>
2025-12-04 15:44:56 +0800

    Update admin.sql

--- admin.sql
+++ admin.sql
@@ -1,1 +1,10 @@
+CREATE TABLE `admin` (
+  `id` double DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `admin_key` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
