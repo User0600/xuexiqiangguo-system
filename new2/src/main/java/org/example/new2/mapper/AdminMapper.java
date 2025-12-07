d3ab265d2a504b67ee5e11e7974da25f8c9efe31
User0600 <143862592+User0600@users.noreply.github.com>
2025-12-07 19:14:19 +0800

    Delete new2 directory

--- new2/src/main/java/org/example/new2/mapper/AdminMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.new2.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.example.new2.entity.Admin;
-@Mapper
-public interface AdminMapper extends BaseMapper<Admin> {
-    // MP 内置了 selectOne, selectById 等，无需手动写 findByUsername
-}
